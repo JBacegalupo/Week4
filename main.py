cf3f2bbea2260551a51ebc4e2ae821b5c87dca54
JBacegalupo <J.BodgerBacegalupo2687@student.leedsbeckett.ac.uk>
2023-10-26 10:23:36 +0100

    pratical 4 code spam

---
+++ main.py
@@ -0,0 +1,48 @@
+import math
+def Task1_SQRT():
+    import math
+    print(math.sqrt(2401))
+
+def Task2_LOG2():
+    from math import log2
+    print(log2(1024))
+
+def displayTwice(msg):
+    """prints a message twice"""
+    print(msg)
+    print(msg)
+
+def findMax(a,b):
+    """Finds the maximum of two values."""
+    if ( a > b ):
+	    max = a
+    else:
+	    max = b
+    return max
+
+def DefualtArgument(num1 , num2 = "?"):
+    if num2 == "?":
+        return num1 * num1
+    else:
+        return num1 * num2
+
+def someFunc(x, y, z):
+	print("x is", x, "\ny is", y, "\nz is", z)
+
+def printDifferentSeperator():
+    print("hello" , "There" , "!", sep="-")
+
+
+def calcAve(*numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total/len(numbers)
+
+
+hypot = lambda a,b : math.sqrt(a * a + b * b)
+
+ToSeconds = lambda a,b = 0 : (a*60*60 + b*60)
+
+print(ToSeconds(2))
+print(ToSeconds(0 , 2))
